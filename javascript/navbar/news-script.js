90fbc24383a1184f8771bc5ac997b7a041a15f92
Lucas Carvalho <lucas.carvalho@guaratibanet.com>
2023-08-22 18:15:54 -0300

    Text array

---
+++ javascript/navbar/news-script.js
@@ -0,0 +1,12 @@
+import NavBar from '../script.js'
+
+class NewsNavbar {
+
+    constructor() {
+         
+    }
+
+
+}
+
+let newsNavbar = new NewsNavbar()
--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,20 @@
-class Test {
+export default class Test {
 
     constructor() {
-        console.log ("Working")
+        
+        this.h3News1 = document.querySelector ("#h3-news1-id")
+        this.pNews1 = document.querySelector ("#p-news1-id")
+
+        this.news1Values = [{},{},{},{}]
+
+        this.news1Values['title'] = `Novo livro em breve`
+        this.news1Values['description'] = `O novo livro já está sendo escrito. Lucas começou a escrever por volta do início de 2022, porém ainda não tem data de lançamento. Segundo o autor, ele escreveu os primeiros cinco capítulos, todavia ainda não os terminou e não tem previsão para terminar.`
+
+        this.h3News1.innerHTML = this.news1Values[0]
+        this.pNews1.innerHTML =  this.news1Values[1]
+
+        console.log (this.news1Values)
+
     }
  
 }
